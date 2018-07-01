ea1100a260038c22cefe301cdc6b4cf80b4b6284
lilacseeking <lilacseeking@outlook.com>
2018-07-01 19:14:28 +0800

    测试分支合并

--- common/src/main/java/org/lilacseeking/common/Controller/TestController.java
+++ common/src/main/java/org/lilacseeking/common/Controller/TestController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/test")
 public class TestController {
-    @RequestMapping(value = "/exam")
+    @RequestMapping(value = "/exam4")
     public String example() {
         return "aaa";
     }
     @RequestMapping(value = "exam2")
     public String example2(){
         return "000";
+    }
+    @RequestMapping(value = "exam3")
+    public String exam3(){
+        return "333";
     }
 }
